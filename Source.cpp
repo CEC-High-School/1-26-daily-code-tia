d268b13fcc944e9cdd28832b16db60d207262fad
Devil-Chan <35504477+Devil-Chan@users.noreply.github.com>
2018-01-26 14:14:17 -0700

    Add files via upload
    e

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int input1;
+	int input2;
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	cout << "Hello! Please input 2 numbers." << endl;
+
+	cin >> input1;
+	cin >> input2;
+	cout << input1 + input2 << endl;
+
+}
